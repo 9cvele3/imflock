f1cc5dfc45c28f1867bcfa2da004c3809dfb98ad
Dusan Cvetkovic <9cvele3@gmail.com>
2025-01-07 14:16:46 +0100

    Adds egui dependency

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use eframe::egui;
+use log::error;
+
+struct ImFlock {
+}
+
+impl ImFlock {
+    fn new() -> Self {
+        Self{
+        }
+    }
+}
+
+impl eframe::App for ImFlock {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            if ui.button("Save graph").clicked() {
+            }
+        });
+
+        ctx.request_repaint();
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+
+    let native_options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([600.0, 400.0]),
+        ..Default::default()
+    };
+
+    eframe::run_native(
+        "ImFlock",
+        native_options,
+        Box::new(move |_cc| Box::new(ImFlock::new())),
+    )
+    .unwrap_or_else(|e| error!("An error occured {}", e));
 }
