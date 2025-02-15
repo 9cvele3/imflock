38ba26e2af1546d5440d8f9bc861c2516663a2be
Dusan Cvetkovic <9cvele3@gmail.com>
2025-02-15 18:14:00 +0100

    Adds img load code

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use eframe::egui;
 use log::error;
+use std::io::BufRead;
+use std::io::Cursor;
+use std::io::Read;
+use std::io::Seek;
 use std::path::PathBuf;
 
 struct ImFlock {
@@ -10,18 +14,18 @@ struct ImFlock {
 
 impl ImFlock {
     fn new() -> Self {
-        Self{
+        Self {
             base_dir: "".into(),
             images: vec![],
             current_img_ind: 0,
         }
     }
-    
-    fn display_img(&self, ctx: &egui::Context, ui: &mut egui::Ui) {
-        let valid_ind = current_img_ind >= 0 && current_img_ind < images.len()
 
-        if valid_ind {
-        }
+    fn display_img(&self, ctx: &egui::Context, ui: &mut egui::Ui) {
+        let valid_ind =
+            self.current_img_ind >= 0 && self.current_img_ind < self.images.len() as u32;
+
+        if valid_ind {}
     }
 }
 
@@ -35,8 +39,6 @@ impl eframe::App for ImFlock {
     }
 }
 
-
-
 fn load_image_from_path(path: &std::path::Path) -> Result<egui::ColorImage, image::ImageError> {
     let mut reader = image::io::Reader::open(path)?;
     load_egui_image_from_image_reader(reader)
@@ -49,12 +51,15 @@ fn load_image_from_bytes(data: &[u8], name: &str) -> Result<egui::ColorImage, im
     load_egui_image_from_image_reader(reader)
 }
 
-fn load_egui_image_from_image_reader<R: Read + BufRead + Seek>(reader: image::io::Reader<R>) -> Result<egui::ColorImage, image::ImageError> {
+fn load_egui_image_from_image_reader<R: Read + BufRead + Seek>(
+    reader: image::io::Reader<R>,
+) -> Result<egui::ColorImage, image::ImageError> {
     let image = reader.decode()?;
     let size = [image.width() as _, image.height() as _];
     let image_buffer = image.to_rgba8();
     let px = image_buffer.as_flat_samples();
-    let pixels = px.as_slice()
+    let pixels = px
+        .as_slice()
         .chunks_exact(4)
         .map(|p| {
             let lr = p[0];
@@ -63,7 +68,7 @@ fn load_egui_image_from_image_reader<R: Read + BufRead + Seek>(reader: image::io
             egui::Color32::from_rgb(lr, lg, lb)
         })
         .collect();
-    let image = egui::ColorImage{size, pixels};
+    let image = egui::ColorImage { size, pixels };
     Ok(image)
 }
 
@@ -82,4 +87,3 @@ fn main() {
     )
     .unwrap_or_else(|e| error!("An error occured {}", e));
 }
-
